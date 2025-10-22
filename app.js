db5227b131ddc8a3cdbc89b64cc56a02f4cd5a11
ismailo02 <muhammadismailcupcake@gmail.com>
2025-10-22 23:36:09 +0500

    Chap(9-13)

---
+++ app.js
@@ -0,0 +1,293 @@
+console.log("Hello World!")
+
+console.log("-----------Chap(9-11)--------------")
+// // ques 1 //
+// // ans 1 //
+
+// let city =prompt("Enter your city.")
+// if (city === "karachi" || city === "Karachi" ){
+//     console.log("Welcome to city of lights.")
+
+// }
+
+// // ques 2 //
+// // ans 2 //
+
+// let gender =prompt("Enter your gender")
+// if (gender ==="Male" || gender ==="male" ){
+//     console.log("Good morning Sir.")
+// }
+// if (gender ==="female" || gender ==="Female"){
+//     console.log("Good morning Ma'am.")
+// }
+
+// ques 3 //
+// ans 3 //
+
+// let trafficLight = prompt("Enter any of the color of Traffic Light")
+
+// if (trafficLight == "Red" || trafficLight == "red" ){
+//     console.log("Must Stop")
+// }
+// else if   (trafficLight == "Yellow" || trafficLight == "yellow" ){
+//     console.log("Ready to move ")
+// }
+// else if (trafficLight == "Green" || trafficLight == "green" ){
+//     console.log("Move on ")
+// }
+// else if  (trafficLight !== "red" && trafficLight !== "yellow" && trafficLight !== "green" ){
+//     console.log("Enter invalid color")
+// }
+
+// ques 4 //
+// ans 4 //
+
+// let fuel =prompt("Enter your fuel in numbers")
+// if (fuel < 0.25){
+//     console.log("Please refill the fuel in your car.")
+// }
+// else{
+//     console.log("Continue driving !")
+// }
+// ques 5 //
+// ans 5 //
+
+// a // 
+// var a =4;
+// if (++a==5){
+// alert("Given condition for variable a is true ")
+// }
+// // b // 
+// var b = 82;
+// if (b++==83){
+// alert("Given condition for variable b is true ")
+// }
+// else(alert("Given condition for variable b is false "))
+// // c //
+// var c = 12;
+// if (c++==13){
+//     alert("Condition 1 for variable c is true ")
+// }
+// if (c==13){
+//       alert("Condition 2 for variable c is true ")
+// }
+// if (++c<13){
+//       alert("Condition 3 for variable c is true ")
+// }
+// if (c==14){
+//       alert("Condition 4 for variable c is true ")
+// }
+// d //
+// var materialCost = 15000;
+// var labourCost = 10000;
+// var totalCost = materialCost+labourCost;
+ 
+// if (totalCost === labourCost + materialCost){
+//     alert("The total cost is "+ totalCost)
+// }
+// // e //
+// if(true){
+//     alert("True")
+// }
+// if(false){
+//     alert("False")
+// }
+// // f //
+// let car  = 500
+// let cat = 5000
+// if (car < cat ){
+//     alert("Car is smaller than cat ")
+// }
+
+// ques 6 //
+// ans 6 //
+
+// let totaltMarks = 100;
+// let marksObtSub1 = +prompt("Enter your Obtained marks for subject 1 ");
+// let marksObtSub2= +prompt("Enter your Obtained marks for subject 2");
+// let marksObtSub3 = +prompt("Enter your Obtained marks for subject 3 ");
+// let MarksObt = marksObtSub1 + marksObtSub2 + marksObtSub3
+// totaltMarks = 100*3
+// let per = (MarksObt/totaltMarks)*100
+// console.log(per,'%')
+// document.writeln("<h1>Mark Sheet </h1>")
+// document.writeln("Total marks =",totaltMarks)
+// document.writeln(" <br>Marks Obtained=",MarksObt)
+// document.writeln("<br>Percantage =",per,'%')
+// if (per >= 80){
+//     let grade = "A -one "
+//     let remarks = "Excellent "
+//     document.writeln("<br>Grade = ",grade)
+//    document.writeln("<br>Remarks = ",remarks)
+// }
+// else if  (per >= 70){
+//     let grade = "A"
+//     let remarks = "Good"
+//     document.writeln("<br>Grade = ",grade)
+//    document.writeln("<br>Remarks = ",remarks)
+// }
+// else if  (per >= 70){
+//     let grade = "B"
+//     let remarks = "You need to improve"
+//     document.writeln("<br>Grade = ",grade)
+//    document.writeln("<br>Remarks = ",remarks)
+// }
+// else if  (per < 60){
+//     let grade = "Fail"
+//     let remarks = "Sorry"
+//     document.writelng("<br>Grade = ",grade)
+//     document.writeln("<br>Remarks = ",remarks)
+// }
+
+ // ques 7 //
+// ans 7 //
+
+// let secretNum = 5;
+// let guessNum = prompt("Guess the number ranging from (1-10)")
+// if (guessNum == secretNum){
+//     alert("Bingo! correct answer")
+// }
+// else if (guessNum == ++secretNum    ){
+//      alert("Close enough to the correct answer")
+// }
+// else{
+//      alert("Wrong Answer!")
+// }
+
+ // ques 8 //
+// ans 8 //
+// let x = prompt("Enter any number")
+// y = x % 3
+// if (y== 0 ){
+//     alert(x+" is divisble by 3 ")
+// }
+// else {
+//     alert(x+" is not  divisble by 3 ")
+// }
+
+ // ques 9 //
+// ans 9 //
+// let even = prompt ("Enter a number")
+// if (even % 2 == 0){
+//     alert(even+" is a even number")
+// }
+// else{
+//     alert(even+" is a odd number")
+// }
+
+ // ques 10 //
+// ans 10 //
+
+// let temp = prompt ("Enter temperature")
+// if (temp > 40){
+//     console.log("It's too hot outisde.")
+// }
+// else if (temp > 30){
+//     console.log("The weather today is normal.")
+// }
+// else if (temp > 20){
+//     console.log("Today's weather is cool.")
+// }
+// else if(temp >= 10){
+//     console.log("OMG! Today's weather is so cool.")
+// }
+
+ // ques 11 //
+// ans 11 //
+
+// let firstNum = 25
+// let secondNum = 25
+// let add = firstNum +secondNum
+// let sub = firstNum - secondNum
+// let mul = firstNum * secondNum
+// let div = firstNum / secondNum
+// let rem = firstNum % secondNum
+
+// if ( add == firstNum + secondNum){
+//     console.log("The result of addition is "+add)
+// }
+//  if (sub == firstNum - secondNum){
+//      console.log ("The result of subraction is "+sub)
+// }
+// if (mul == firstNum * secondNum){
+//   console.log ("The result of multiplication is "+mul)
+// }
+//  if (div == firstNum / secondNum){
+//      console.log ("The result of division  is "+div)
+// }
+//  if (rem == firstNum % secondNum){
+//     console.log ("The resultof modulus is "+rem)
+// }
+
+console.log("-----------Chap(12-13)--------------")
+
+ // ques 2 //
+ // ans 2 //
+ 
+//  let numFirst = prompt("Enter a number")
+//  let numSec = prompt("Enter a number")
+//  if (numFirst>numSec){
+//     alert("first number is larger ")
+//  }
+//  else if (numFirst<numSec){
+//     alert("Second number is larger ")
+//  }
+//  else if (numFirst==numSec){
+//     alert("Both numbers are equal")
+//  }
+  // ques 3 //
+ // ans 3 //
+
+// let y = prompt ("Enter a number")
+// if (y>0){
+//     alert("The number is positive")
+// }
+// else if (y<0){
+//      alert("The number is negative")
+// }
+// else if (y==0){
+//     alert("The number is zero")
+// }
+ // ques 4 //
+ // ans 4 //
+
+//  let char = prompt("Enter a vowel")
+//  if (char == "a" || char == "e"|| char == "i"|| char == "o"|| char == "u" ){
+//     alert(true)
+//  }
+//  else{
+//     alert(false)
+//  }
+
+
+
+ // ques 5 //
+ // ans 5 //
+
+// let correctPass = "java55"
+// let user = prompt("Enter your password")
+// if (user === ""){
+//     alert("Please enter your password")
+// }
+// else if (user == correctPass){
+//     alert("Correct! The password you entered matches the original password")
+// }
+// else {
+//  alert("Incorrect password!") 
+// }
+
+
+
+ // ques 6 //
+// ans 6 //
+
+// var greeting;
+// var hour = 15
+// if (hour < 18){
+//     greeting="Good Day";
+//     alert(greeting)
+// }
+// else {
+//     greeting="Good Evening";
+//     alert(greeting)
+// }
